ffef87f28fe9805c7cb6529de8e1003ec7e19b00
Ruth Ramya <bhavani.bolloju@gmail.com>
2023-05-18 10:59:37 +0530

    refactor code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,40 @@ function Navbar() {
   return (
     <nav className={classes["navbar"]}>
       <div className={classes["navbar__logo"]}>edyoda</div>
-      <div className={classes["navbar__options"]}>courses</div>
-      <div className={classes["navbar__options"]}>programs</div>
+      <div className={`${classes["navbar__options"]} ${classes["option"]}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+        <span>courses</span>
+      </div>
+      <div className={`${classes["navbar__options"]} ${classes["option"]}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+        <span>programs</span>
+      </div>
 
       <div className={classes["navbar__search"]}>
         <input type="text" />
